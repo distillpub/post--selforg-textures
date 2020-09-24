3d8c74433cfd612eaaa229169e4d37b93fb2223a
Eyvind Niklasson <eyvind@google.com>
2020-09-24 14:00:17 +0200

    update build script to work with GHPages

--- bin/dev.py
+++ bin/dev.py
@@ -8,48 +8,124 @@ if six.PY3:
     from http.server import SimpleHTTPRequestHandler, test
 else:
     from SimpleHTTPServer import SimpleHTTPRequestHandler, test
+import re
+
+def copy_byte_range(infile, outfile, start=None, stop=None, bufsize=16*1024):
+    '''Like shutil.copyfileobj, but only copy a range of the streams.
+    Both start and stop are inclusive.
+    '''
+    if start is not None: infile.seek(start)
+    while 1:
+        to_read = min(bufsize, stop + 1 - infile.tell() if stop else bufsize)
+        buf = infile.read(to_read)
+        if not buf:
+            break
+        outfile.write(buf)
+
+
+BYTE_RANGE_RE = re.compile(r'bytes=(\d+)-(\d+)?$')
+def parse_byte_range(byte_range):
+    '''Returns the two numbers in 'bytes=123-456' or throws ValueError.
+    The last number or both numbers may be None.
+    '''
+    if byte_range.strip() == '':
+        return None, None
+
+    m = BYTE_RANGE_RE.match(byte_range)
+    if not m:
+        raise ValueError('Invalid byte range %s' % byte_range)
+
+    first, last = [x and int(x) for x in m.groups()]
+    if last and last < first:
+        raise ValueError('Invalid byte range %s' % byte_range)
+    return first, last
 
 def write_file(fname, fout):
     for s in open(fname):
-        if "___MODELS___" in s:
-            fout.write(s.replace("___MODELS___", str(glob.glob("*.json"))))
-            continue
-        if s.startswith('%% '):
-            inc = s.split()
-            if len(inc) == 4:
-                # auto update this file from specified git repo
-                update_git_file(*inc[1:])
-            fn = '../'+inc[1]
+        if s.strip().startswith('{% include '):
+            fn = '../'+s.split()[2]
             write_file(fn, fout)
         else:
             fout.write(s)
 
-def update_git_file(path, gitUsername, gitRepo):
+def build():
     if os.environ.get("GIT_API_KEY_SELFORG") is not None:
-        print("Using api key in environment")
-        os.system('''curl -o ../''' + path + ''' \
+        print("using api key in environment")
+        os.system('''curl -o ../article.html \
         --header 'Authorization: token ''' + os.environ.get("GIT_API_KEY_SELFORG") + '''' \
         --header 'Accept: application/vnd.github.v3.raw' \
-        --location https://api.github.com/repos/''' + gitUsername + '''/''' + gitRepo + '''/contents/''' + path + ''' \
+        --location https://api.github.com/repos/$(git remote -v | head -n 1 | sed 's/.*github\.com:\(.*\)\.git.*/\\1/')/contents/article.html \
         ''')
     else:
-        print("No api key available to auto-download file %s. Please provide it by setting ENV var GIT_API_KEY_SELFORG=token" % path)
-
-def build():
+        print("no api key available")
     with open('index.html', 'w') as fout:
       write_file('../main.html', fout)
     print('build finished')
 
 
 class Handler(SimpleHTTPRequestHandler):
+
     def do_GET(self):
         if self.path in ['/', '/index.html']:
             build()
         if six.PY3:
             super().do_GET()
         else:
-            print(self)
             SimpleHTTPRequestHandler.do_GET(self)
+
+    # From https://github.com/danvk/RangeHTTPServer.
+    # Safari requires Range to be supported for video files.
+    def send_head(self):
+        if 'Range' not in self.headers:
+            self.range = None
+            return SimpleHTTPRequestHandler.send_head(self)
+        try:
+            self.range = parse_byte_range(self.headers['Range'])
+        except ValueError as e:
+            self.send_error(400, 'Invalid byte range')
+            return None
+        first, last = self.range
+
+        # Mirroring SimpleHTTPServer.py here
+        path = self.translate_path(self.path)
+        f = None
+        ctype = self.guess_type(path)
+        try:
+            f = open(path, 'rb')
+        except IOError:
+            self.send_error(404, 'File not found')
+            return None
+
+        fs = os.fstat(f.fileno())
+        file_len = fs[6]
+        if first >= file_len:
+            self.send_error(416, 'Requested Range Not Satisfiable')
+            return None
+
+        self.send_response(206)
+        self.send_header('Content-type', ctype)
+        self.send_header('Accept-Ranges', 'bytes')
+
+        if last is None or last >= file_len:
+            last = file_len - 1
+        response_length = last - first + 1
+
+        self.send_header('Content-Range',
+                         'bytes %s-%s/%s' % (first, last, file_len))
+        self.send_header('Content-Length', str(response_length))
+        self.send_header('Last-Modified', self.date_time_string(fs.st_mtime))
+        self.end_headers()
+        return f
+
+    def copyfile(self, source, outputfile):
+        if not self.range:
+            return SimpleHTTPRequestHandler.copyfile(self, source, outputfile)
+
+        # SimpleHTTPRequestHandler uses shutil.copyfileobj, which doesn't let
+        # you stop the copying before the end of the file.
+        start, stop = self.range  # set in send_head()
+        copy_byte_range(source, outputfile, start, stop)
+    
 
 if __name__ == '__main__':
     os.chdir('public')
